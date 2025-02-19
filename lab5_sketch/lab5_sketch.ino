8de9eb037c5045d14cfac1e14d3a0cd712f9040b
unknown <bisharabrian2@gmail.com>
2025-02-19 13:27:09 -0600

    copied lab4 script

--- lab5_sketch/lab5_sketch.ino
+++ lab5_sketch/lab5_sketch.ino
@@ -1,9 +1,51 @@
+const int trigPin = 11;
+const int echoPin = 12;
+long duration;
+int distanceCm, distanceInch;
+
 void setup() {
-  // put your setup code here, to run once:
+
+  // peripheral pins
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  // LED pins
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  // Sending trigger pulse
+  // Sending trigger pulse to peripheral
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); // good practice to send low state to peripheral first
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Calculation output pulse duration
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calibrating sensor
+@@ -30,6 +37,17 @@
+  Serial.print(" cm/");
+  Serial.print(distanceInch);
+  Serial.println(" in");
+
+  // LED feedback for distance
+    if (distanceInch > 10) {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  } 
+  else {
+    digitalWrite(4, HIGH);
+    digitalWrite(3, LOW);   
+  }
+
+  delay(1000);
 
 }
