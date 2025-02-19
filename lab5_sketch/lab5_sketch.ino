d428d2d39587a96e851ad2533b32fee13414b0d6
unknown <bisharabrian2@gmail.com>
2025-02-19 15:06:42 -0600

    added comments

--- lab5_sketch/lab5_sketch.ino
+++ lab5_sketch/lab5_sketch.ino
@@ -1,14 +1,14 @@
 #include <ArduinoBLE.h>
 
-BLEService newService("180A"); // creating the service
-BLEByteCharacteristic readChar("2A57", BLERead);
-BLEByteCharacteristic writeChar("2A58", BLEWrite);
+BLEService newService("180A"); // create the service
+BLEByteCharacteristic readChar("2A57", BLERead); // create the read characteristic
+BLEByteCharacteristic writeChar("2A58", BLEWrite); // create the write characteristic
 
+// peripheral pins
 const int trigPin = 8;
 const int echoPin = 10;
 
 long previousMillis = 0;
-
 long duration;
 int distanceCm, distanceInch;
 
@@ -20,21 +20,23 @@ void setup() {
 
   Serial.begin(9600);
 
-  while(!Serial);
-  if (!BLE.begin()){
+  while(!Serial); // must open serial monitor
+  if (!BLE.begin()){ // pause function if ArduinoBLE hasn't finished initializing
     Serial.println("Waiting for ArduinoBLE");
     while(1);
   }
 
   BLE.setLocalName("Brian_Cameron_Akshay");
-  BLE.setAdvertisedService(newService);
-  newService.addCharacteristic(readChar);
-  newService.addCharacteristic(writeChar);
-  BLE.addService(newService);
+  BLE.setAdvertisedService(newService); // add service as discoverable
+  newService.addCharacteristic(readChar); // add read characteristic
+  newService.addCharacteristic(writeChar); // add write characteristic
+  BLE.addService(newService); // add service to device
 
+  // initialize characteristic values
   readChar.writeValue(0);
   writeChar.writeValue(0);
 
+  // confirm that device is properly set up and advertised
   BLE.advertise();
   Serial.println("Bluetooth device active");
 
@@ -46,7 +48,6 @@ void loop() {
 
   if (central) {  // if a central is connected to the peripheral
     Serial.print("Connected to central: ");
-    
     Serial.println(central.address()); // print the central's BT address
     
     digitalWrite(LED_BUILTIN, HIGH); // turn on the LED to indicate the connection
@@ -57,20 +58,21 @@ void loop() {
       if (currentMillis - previousMillis >= 200) { 
         previousMillis = currentMillis;
 
+        // trigger sensor only when central writes to peripheral
         if (writeChar.written()) {
           if (writeChar.value()) {
 
-            // Sending trigger pulse to peripheral
+            // send trigger pulse to peripheral
             digitalWrite(trigPin, LOW);
             delayMicroseconds(2); // good practice to send low state to peripheral first
             digitalWrite(trigPin, HIGH);
             delayMicroseconds(10);
             digitalWrite(trigPin, LOW);
 
-            // Calculation output pulse duration
+            // calculate output pulse duration
             duration = pulseIn(echoPin, HIGH);
 
-            // Calibrating sensor
+            // calibrate sensor
             distanceCm = duration / 58;
             distanceInch = duration / 148;
             Serial.print("Distance: ");
@@ -80,12 +82,12 @@ void loop() {
             Serial.println(" in");
             delay(10);
 
+            // read distance value from peripheral
             readChar.writeValue(distanceCm);
             Serial.println("Distance printed to peripheral");
 
          }
         }
-
       }
     }
 
